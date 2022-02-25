7e7b3fe6b833ffe55516e437c335f115932aa44f
Iftekhar E Mahbub Zeeon <1805038@ugrad.cse.buet.ac.bd>
2022-02-25 16:02:14 +0600

    Server info removed

--- serverInfomation.js
+++
@@ -1,9 +0,0 @@
-const dbPassword = 'admin';
-const dbUser = 'C##911_WEB_PORTAL';
-const connectionString = 'localhost:1521/ORCL';
-
-module.exports = {
-    dbPassword,
-    dbUser,
-    connectionString
-}
