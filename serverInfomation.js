97f04a767f660c12567e5562804e2481b2dfc616
Iftekhar E Mahbub Zeeon <1805038@ugrad.cse.buet.ac.bd>
2022-02-25 16:05:30 +0600

    Server updated

---
+++ serverInfomation.js
@@ -0,0 +1,9 @@
+const dbPassword = 'admin';
+const dbUser = 'C##911_WEB_PORTAL';
+const connectionString = 'localhost:1521/ORCL';
+
+module.exports = {
+    dbPassword,
+    dbUser,
+    connectionString
+}
